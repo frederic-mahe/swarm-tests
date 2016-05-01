10983a14ba9ac0fbc8b4d08ce5c08d90d3a439aa
Frédéric Mahé <mahe@rhrk.uni-kl.de>
2016-05-01 11:16:27 +0200

    Complete set of tests for the main options

--- scripts/test_options.sh
+++ scripts/test_options.sh
@@ -9,10 +9,11 @@ printf "# %s %s\n" "${LINE:${#SCRIPT_NAME}}" "${SCRIPT_NAME}"
 RED="\033[1;31m"
 GREEN="\033[1;32m"
 NO_COLOR="\033[0m"
+NULL="/dev/null"
 
 failure () {
     printf "${RED}FAIL${NO_COLOR}: ${1}\n"
-    exit -1
+    # exit -1
 }
 
 success () {
@@ -40,11 +41,28 @@ DESCRIPTION="check if swarm is in the PATH"
 ## Return status should be 0 after -h and -v (GNU standards)
 for OPTION in "-h" "--help" "-v" "--version" ; do
     DESCRIPTION="return status should be 0 after ${OPTION}"
-    "${SWARM}" "${OPTION}" 2> /dev/null && \
+    "${SWARM}" "${OPTION}" 2> "${NULL}" && \
         success "${DESCRIPTION}" || \
             failure "${DESCRIPTION}"
 done
 
+#*****************************************************************************#
+#                                                                             #
+#                                  No option                                  #
+#                                                                             #
+#*****************************************************************************#
+
+## No option, only standard input
+DESCRIPTION="swarm runs normally when no option is specified (data on stdin)"
+"${SWARM}" < "${ALL_IDENTICAL}" > "${NULL}" 2> "${NULL}" && \
+    success "${DESCRIPTION}" || \
+        failure "${DESCRIPTION}"
+
+## No option, only a fasta file
+DESCRIPTION="swarm runs normally when no option is specified (data in file)"
+"${SWARM}" "${ALL_IDENTICAL}" > "${NULL}" 2> "${NULL}" && \
+    success "${DESCRIPTION}" || \
+        failure "${DESCRIPTION}"
 
 #*****************************************************************************#
 #                                                                             #
@@ -52,48 +70,42 @@ done
 #                                                                             #
 #*****************************************************************************#
 
-## Number of threads (--threads is not specified)
-DESCRIPTION="swarm runs normally when --threads is not specified"
-"${SWARM}" < "${ALL_IDENTICAL}" > /dev/null 2> /dev/null && \
-    success "${DESCRIPTION}" || \
-        failure "${DESCRIPTION}"
-
 ## Number of threads (--threads from 1 to 256)
 MIN=1
 MAX=256
 DESCRIPTION="swarm runs normally when --threads goes from ${MIN} to ${MAX}"
 for ((t=$MIN ; t<=$MAX ; t++)) ; do
-    "${SWARM}" -t ${t} < "${ALL_IDENTICAL}" > /dev/null 2> /dev/null || \
+    "${SWARM}" -t ${t} < "${ALL_IDENTICAL}" > "${NULL}" 2> "${NULL}" || \
         failure "swarm aborts when --threads equals ${t}"
 done && success "${DESCRIPTION}"
 
 ## Number of threads (--threads is empty)
 DESCRIPTION="swarm aborts when --threads is empty"
-"${SWARM}" -t < "${ALL_IDENTICAL}" 2> /dev/null && \
+"${SWARM}" -t < "${ALL_IDENTICAL}" 2> "${NULL}" && \
     failure "${DESCRIPTION}" || \
         success "${DESCRIPTION}"
 
 ## Number of threads (--threads is zero)
 DESCRIPTION="swarm aborts when --threads is zero"
-"${SWARM}" -t 0 < "${ALL_IDENTICAL}" 2> /dev/null && \
+"${SWARM}" -t 0 < "${ALL_IDENTICAL}" 2> "${NULL}" && \
     failure "${DESCRIPTION}" || \
         success "${DESCRIPTION}"
 
 ## Number of threads (--threads is 257)
 DESCRIPTION="swarm aborts when --threads is 257"
-"${SWARM}" -t 257 < "${ALL_IDENTICAL}" 2> /dev/null && \
+"${SWARM}" -t 257 < "${ALL_IDENTICAL}" 2> "${NULL}" && \
     failure "${DESCRIPTION}" || \
         success "${DESCRIPTION}"
 
 ## Number of threads (number of threads is way too large)
 DESCRIPTION="swarm aborts when --threads is 8 billions"
-"${SWARM}" -t 8000000000 < "${ALL_IDENTICAL}" 2> /dev/null && \
+"${SWARM}" -t 8000000000 < "${ALL_IDENTICAL}" 2> "${NULL}" && \
     failure "${DESCRIPTION}" || \
         success "${DESCRIPTION}"
 
 ## Number of threads (--threads is non-numerical)
 DESCRIPTION="swarm aborts when --threads is not numerical"
-"${SWARM}" -t "a" < "${ALL_IDENTICAL}" 2> /dev/null && \
+"${SWARM}" -t "a" < "${ALL_IDENTICAL}" 2> "${NULL}" && \
     failure "${DESCRIPTION}" || \
         success "${DESCRIPTION}"
 
@@ -109,34 +121,254 @@ MIN=0
 MAX=256
 DESCRIPTION="swarm runs normally when --differences goes from ${MIN} to ${MAX}"
 for ((d=$MIN ; d<=$MAX ; d++)) ; do
-    "${SWARM}" -d ${d} < "${ALL_IDENTICAL}" > /dev/null 2> /dev/null || \
+    "${SWARM}" -d ${d} < "${ALL_IDENTICAL}" > "${NULL}" 2> "${NULL}" || \
         failure "swarm aborts when --differences equals ${d}"
 done && success "${DESCRIPTION}"
 
 ## Number of differences (--difference is empty)
 DESCRIPTION="swarm aborts when --difference is empty"
-"${SWARM}" -d < "${ALL_IDENTICAL}" 2> /dev/null && \
+"${SWARM}" -d < "${ALL_IDENTICAL}" 2> "${NULL}" && \
+    failure "${DESCRIPTION}" || \
+        success "${DESCRIPTION}"
+
+## Number of differences (--differences is negative)
+DESCRIPTION="swarm aborts when --difference is -1"
+"${SWARM}" -d -1 < "${ALL_IDENTICAL}" 2> "${NULL}" && \
     failure "${DESCRIPTION}" || \
         success "${DESCRIPTION}"
 
 ## Number of differences (--differences is 257)
 DESCRIPTION="swarm aborts when --difference is 257"
-"${SWARM}" -d 257 < "${ALL_IDENTICAL}" 2> /dev/null && \
+"${SWARM}" -d 257 < "${ALL_IDENTICAL}" > "${NULL}" 2> "${NULL}" && \
     failure "${DESCRIPTION}" || \
         success "${DESCRIPTION}"
 
 ## Number of differences (number of differences is way too large)
 DESCRIPTION="swarm aborts when --difference is 8 billions"
-"${SWARM}" -d 8000000000 < "${ALL_IDENTICAL}" 2> /dev/null && \
+"${SWARM}" -d 8000000000 < "${ALL_IDENTICAL}" > "${NULL}" 2> "${NULL}" && \
     failure "${DESCRIPTION}" || \
         success "${DESCRIPTION}"
 
 ## Number of differences (--difference is non-numerical)
 DESCRIPTION="swarm aborts when --difference is not numerical"
-"${SWARM}" -d "a" < "${ALL_IDENTICAL}" 2> /dev/null && \
+"${SWARM}" -d "a" < "${ALL_IDENTICAL}" 2> "${NULL}" && \
     failure "${DESCRIPTION}" || \
         success "${DESCRIPTION}"
 
+
+#*****************************************************************************#
+#                                                                             #
+#                             Fastidious options                              #
+#                                                                             #
+#*****************************************************************************#
+
+## Swarm should run normally when the fastidious option is specified
+DESCRIPTION="swarm runs normally when the fastidious option is specified"
+"${SWARM}" -f < "${ALL_IDENTICAL}" > "${NULL}" 2> "${NULL}" && \
+    success "${DESCRIPTION}" || \
+        failure "${DESCRIPTION}"
+
+## Boundary -------------------------------------------------------------------
+
+## Boundary (-b is empty)
+DESCRIPTION="swarm aborts when --boundary is empty"
+"${SWARM}" -f -b < "${ALL_IDENTICAL}" 2> "${NULL}" && \
+    failure "${DESCRIPTION}" || \
+        success "${DESCRIPTION}"
+
+## Boundary (-b is negative)
+DESCRIPTION="swarm aborts when --boundary is -1"
+"${SWARM}" -f -b -1 < "${ALL_IDENTICAL}" > "${NULL}" 2> "${NULL}" && \
+    failure "${DESCRIPTION}" || \
+        success "${DESCRIPTION}"
+
+## Boundary (-b is non-numerical)
+DESCRIPTION="swarm aborts when --boundary is not numerical"
+"${SWARM}" -f -b "a" < "${ALL_IDENTICAL}" 2> "${NULL}" && \
+    failure "${DESCRIPTION}" || \
+        success "${DESCRIPTION}"
+
+## Boundary (-b == 1)
+DESCRIPTION="swarm aborts when --boundary is 1"
+"${SWARM}" -f -b 1 < "${ALL_IDENTICAL}" > "${NULL}" 2> "${NULL}" && \
+    failure "${DESCRIPTION}" || \
+        success "${DESCRIPTION}"
+
+## Accepted values for the boundary option are integers > 1
+MIN=2
+MAX=255
+DESCRIPTION="swarm runs normally when --boundary goes from ${MIN} to ${MAX}"
+for ((b=$MIN ; b<=$MAX ; b++)) ; do
+    "${SWARM}" -f -b ${b} < "${ALL_IDENTICAL}" > "${NULL}" 2> "${NULL}" || \
+        failure "swarm aborts when --boundary equals ${b}"
+done && success "${DESCRIPTION}"
+
+## Passing the --boundary option without the fastidious option should fail
+DESCRIPTION="swarm fails when the boundary option is specified without -f"
+"${SWARM}" -b 3 < "${ALL_IDENTICAL}" > "${NULL}" 2> "${NULL}" && \
+    failure "${DESCRIPTION}" || \
+        success "${DESCRIPTION}"
+
+## Ceiling --------------------------------------------------------------------
+
+## Ceiling (-c is empty)
+DESCRIPTION="swarm aborts when --ceiling is empty"
+"${SWARM}" -f -c < "${ALL_IDENTICAL}" 2> "${NULL}" && \
+    failure "${DESCRIPTION}" || \
+        success "${DESCRIPTION}"
+
+## Ceiling (-c is negative)
+DESCRIPTION="swarm aborts when --ceiling is -1"
+"${SWARM}" -f -c -1 < "${ALL_IDENTICAL}" > "${NULL}" 2> "${NULL}" && \
+    failure "${DESCRIPTION}" || \
+        success "${DESCRIPTION}"
+
+## Ceiling (-c is non-numerical)
+DESCRIPTION="swarm aborts when --ceiling is not numerical"
+"${SWARM}" -f -c "a" < "${ALL_IDENTICAL}" 2> "${NULL}" && \
+    failure "${DESCRIPTION}" || \
+        success "${DESCRIPTION}"
+
+## Ceiling (-c == 0)
+DESCRIPTION="swarm aborts when --ceiling is 0"
+"${SWARM}" -f -c 0 < "${ALL_IDENTICAL}" > "${NULL}" 2> "${NULL}" && \
+    failure "${DESCRIPTION}" || \
+        success "${DESCRIPTION}"
+
+## Accepted values for the ceiling option are positive integers
+MIN=1
+MAX=255
+DESCRIPTION="swarm runs normally when --ceiling goes from ${MIN} to ${MAX}"
+for ((c=$MIN ; c<=$MAX ; c++)) ; do
+    "${SWARM}" -f -c ${c} < "${ALL_IDENTICAL}" > "${NULL}" 2> "${NULL}" || \
+        failure "swarm aborts when --ceiling equals ${c}"
+done && success "${DESCRIPTION}"
+
+## Passing the --ceiling option without the fastidious option should fail
+DESCRIPTION="swarm fails when the ceiling option is specified without -f"
+"${SWARM}" -c 10 < "${ALL_IDENTICAL}" > "${NULL}" 2> "${NULL}" && \
+    failure "${DESCRIPTION}" || \
+        success "${DESCRIPTION}"
+
+## Bloom bits -----------------------------------------------------------------
+
+## Bloom bits (-y is empty)
+DESCRIPTION="swarm aborts when --bloom-bits is empty"
+"${SWARM}" -f -y < "${ALL_IDENTICAL}" 2> "${NULL}" && \
+    failure "${DESCRIPTION}" || \
+        success "${DESCRIPTION}"
+
+## Bloom bits (-y is negative)
+DESCRIPTION="swarm aborts when --bloom-bits is -1"
+"${SWARM}" -f -y -1 < "${ALL_IDENTICAL}" > "${NULL}" 2> "${NULL}" && \
+    failure "${DESCRIPTION}" || \
+        success "${DESCRIPTION}"
+
+## Bloom bits (-y is non-numerical)
+DESCRIPTION="swarm aborts when --bloom-bits is not numerical"
+"${SWARM}" -f -y "a" < "${ALL_IDENTICAL}" 2> "${NULL}" && \
+    failure "${DESCRIPTION}" || \
+        success "${DESCRIPTION}"
+
+## Accepted values for the --bloom-bits option goes from 2 to 64
+MIN=2
+MAX=64
+DESCRIPTION="swarm runs normally when --bloom-bits goes from ${MIN} to ${MAX}"
+for ((y=$MIN ; y<=$MAX ; y++)) ; do
+    "${SWARM}" -f -y ${y} < "${ALL_IDENTICAL}" > "${NULL}" 2> "${NULL}" || \
+        failure "swarm aborts when --bloom-bits equals ${y}"
+done && success "${DESCRIPTION}"
+
+## Rejected values for the --bloom-bits option are < 2
+DESCRIPTION="swarm aborts when --bloom-bits is lower than 2"
+for y in 0 1 ; do
+    "${SWARM}" -f -y ${y} < "${ALL_IDENTICAL}" > "${NULL}" 2> "${NULL}" && \
+        failure "swarm runs normally when --bloom-bits equals ${y}"
+done || success "${DESCRIPTION}"
+
+## Accepted values for the --bloom-bits option goes from 2 to 64
+MIN=65
+MAX=255
+DESCRIPTION="swarm aborts when --bloom-bits is higher than 64"
+for ((y=$MIN ; y<=$MAX ; y++)) ; do
+    "${SWARM}" -f -y ${y} < "${ALL_IDENTICAL}" > "${NULL}" 2> "${NULL}" && \
+        failure "swarm runs normally when --bloom-bits equals ${y}"
+done || success "${DESCRIPTION}"
+
+## Passing the --bloom-bits option without the fastidious option should fail
+DESCRIPTION="swarm fails when the --bloom-bits option is specified without -f"
+"${SWARM}" -y 16 < "${ALL_IDENTICAL}" > "${NULL}" 2> "${NULL}" && \
+    failure "${DESCRIPTION}" || \
+        success "${DESCRIPTION}"
+
+
+#*****************************************************************************#
+#                                                                             #
+#                     Pairwise alignment advanced options                     #
+#                                                                             #
+#*****************************************************************************#
+
+while read LONG SHORT ; do
+    ## Using option when d = 1 should fail (or warning?)
+    DESCRIPTION="swarm fails when --${LONG} is specified and d = 1"
+    "${SWARM}" -d 1 "${OPTION}" 1 < "${ALL_IDENTICAL}" \
+               > "${NULL}" 2> "${NULL}" && \
+        failure "${DESCRIPTION}" || \
+            success "${DESCRIPTION}"
+
+    ## option is empty
+    DESCRIPTION="swarm aborts when --${LONG} is empty"
+    "${SWARM}" -d 2 ${SHORT} < "${ALL_IDENTICAL}" 2> "${NULL}" && \
+        failure "${DESCRIPTION}" || \
+            success "${DESCRIPTION}"
+
+    ## option is negative
+    DESCRIPTION="swarm aborts when --${LONG} is -1"
+    "${SWARM}" -d 2 ${SHORT} -1 < "${ALL_IDENTICAL}" \
+               > "${NULL}" 2> "${NULL}" && \
+        failure "${DESCRIPTION}" || \
+            success "${DESCRIPTION}"
+
+    ## option is non-numerical
+    DESCRIPTION="swarm aborts when --${LONG} is not numerical"
+    "${SWARM}" -d 2 ${SHORT} "a" < "${ALL_IDENTICAL}" 2> "${NULL}" && \
+        failure "${DESCRIPTION}" || \
+            success "${DESCRIPTION}"
+
+    ## option is negative (allowed for -m & -p, not for -g & -e)
+    if [[ "${SHORT}" == "-m" || "${SHORT}" == "-p" ]] ; then
+        DESCRIPTION="swarm aborts when --${LONG} is null"
+        "${SWARM}" -d 2 ${SHORT} 0 < "${ALL_IDENTICAL}" \
+                   > "${NULL}" 2> "${NULL}" && \
+            failure "${DESCRIPTION}" || \
+                success "${DESCRIPTION}"
+    elif [[ "${SHORT}" == "-g" || "${SHORT}" == "-e" ]] ; then
+        DESCRIPTION="swarm runs normally when --${LONG} is null"
+        "${SWARM}" -d 2 ${SHORT} 0 < "${ALL_IDENTICAL}" \
+                   > "${NULL}" 2> "${NULL}" && \
+            success "${DESCRIPTION}" || \
+                failure "${DESCRIPTION}"
+    else
+        DESCRIPTION="unknown option"
+        failure "${DESCRIPTION}"
+    fi
+    
+    ## Accepted values for the option goes from 1 to 255
+    MIN=1
+    MAX=255
+    DESCRIPTION="swarm runs normally when --${LONG} goes from ${MIN} to ${MAX}"
+    for ((i=$MIN ; i<=$MAX ; i++)) ; do
+        "${SWARM}" -d 2 "${SHORT}" ${i} < "${ALL_IDENTICAL}" > "${NULL}" 2> "${NULL}" || \
+            failure "swarm aborts when --${LONG} equals ${i}"
+    done && success "${DESCRIPTION}"
+    
+done <<EOF
+match-reward -m
+mismatch-penalty -p
+gap-opening-penalty -g
+gap-extension-penalty -e
+EOF
+    
 ## Clean
 rm "${ALL_IDENTICAL}"
 
