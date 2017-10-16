9a2635a03c7fc6a6a6e4cd62a6a3f3ced2ebd22a
Frédéric Mahé <mahe@rhrk.uni-kl.de>
2017-10-16 18:22:53 +0200

    capture the text written by swarm when it is waiting for data
    That was tricky. In order to eliminate a shell message saying that the
    process is terminated, I had to use the -PIPE option of the kill
    command. That script may not be portable.

--- scripts/fixed_bugs.sh
+++ scripts/fixed_bugs.sh
@@ -1859,12 +1859,14 @@ printf ">s1_%d\nA\n" $(( 1 << 32 )) | \
 ##
 ## swarm waits when it receives no input data (no file, no pipe, no
 ## redirection). That's normal. See also issue 36.
+OUTPUT=$(mktemp)
 DESCRIPTION="issue 105 --- when no filename is given, swarm says it is waiting for data on stdin"
-printf ">s1_1\nA\n" | \
-    "${SWARM}" > /dev/null 2>&1 | \
-    grep -q "^waiting for data" && \
+(cmdpid=${BASHPID}
+ (sleep 1 ; kill -PIPE ${cmdpid} &> /dev/null) & "${SWARM}" 2> "${OUTPUT}")
+grep -q "^Waiting" "${OUTPUT}" && \
     success "${DESCRIPTION}" || \
         failure "${DESCRIPTION}"
+rm "${OUTPUT}"
 
 
 #*****************************************************************************#
